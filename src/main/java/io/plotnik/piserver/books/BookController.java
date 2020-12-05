f32fbe8c7113738e771a17a9e5ca00ed549a3ae7
Egor Abramovich <eabramovich@exadel.com>
2020-12-05 15:50:01 +0300

    book counter

--- src/main/java/io/plotnik/piserver/books/BookController.java
+++ src/main/java/io/plotnik/piserver/books/BookController.java
@@ -10,26 +10,32 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 public class BookController {
 
+    private static final Logger log = LoggerFactory.getLogger(BookController.class);
+
     @Value("${home.path}")
-    private String homePath;  
+    private String homePath;
 
     @Value("${lists.path}")
-    private String listsPath;  
+    private String listsPath;
 
     @RequestMapping(value="/books", method=RequestMethod.GET)
     public Book[] getList() {
         try {
             byte[] jsonData = Files.readAllBytes(Paths.get(homePath, listsPath, "books.json"));
             ObjectMapper mapper = new ObjectMapper();
-            Book[] books = mapper.readValue(jsonData, Book[].class);  
-            return books; 
-            
+            Book[] books = mapper.readValue(jsonData, Book[].class);
+            log.info(books.length + " books");
+            return books;
+
         } catch (IOException e) {
             e.printStackTrace();
             return null;
-        }     
-    }    
+        }
+    }
 }
